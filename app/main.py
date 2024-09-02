8e559e0b5f902f64154fa7b3475160024986e31c
Mohit Jain <mohit.jain3099@gmail.com>
2024-09-01 23:57:13 -0700

    Committing braces

--- app/main.py
+++ app/main.py
@@ -29,6 +29,10 @@ def main():
             print("LEFT_PAREN ( null")
         if c == ")":
             print("RIGHT_PAREN ) null")
+        if c == "{":
+            print("LEFT_BRACE { null")
+        if c == "}":
+            print("RIGHT_BRACE } null")
     print("EOF  null")
 
 
