8af950d09a44f17879dd443bb70b329b7bc49aba
Mohit Jain <mohit.jain3099@gmail.com>
2024-10-06 12:51:23 -0400

    grouping None

--- app/main.py
+++ app/main.py
@@ -290,6 +290,8 @@ class Grouping:
     def __init__(self, expression):
         self.expression = expression
     def __str__(self):
+        if self.expression is None:
+            return ""
         return f"(group {self.expression})"
     def __repr__(self):
         return str(self)
